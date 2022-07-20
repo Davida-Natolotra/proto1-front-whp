c01af726ed5b4b0c4d22792c065a9e91b77b12eb
DavidaNatolotra <r.davida.natolotra@gmail.com>
2022-07-20 09:32:20 +0300

    local

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,7 @@ import Grid from "@mui/material/Grid";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Logo from "../Logo-Ezaho.svg";
 
 function Copyright(props) {
 	return (
@@ -22,17 +23,17 @@ function Copyright(props) {
 			{...props}
 		>
 			{"Copyright © "}
-			<Link color="inherit" href="https://mui.com/">
-				Your Website
+			<Link color="inherit" href="https://e-zaho.mg">
+				e-zaho
 			</Link>
-			{new Date().getFullYear()}
+			{" " + new Date().getFullYear()}
 		</Typography>
 	);
 }
 
 const theme = createTheme();
 
-export default function SignInSide() {
+export default function HomePage() {
 	const handleSubmit = (event) => {
 		event.preventDefault();
 		const data = new FormData(event.currentTarget);
@@ -52,13 +53,13 @@ export default function SignInSide() {
 					sm={4}
 					md={7}
 					sx={{
-						backgroundImage: "url(https://source.unsplash.com/random)",
+						backgroundImage: `url(${Logo})`,
 						backgroundRepeat: "no-repeat",
 						backgroundColor: (t) =>
 							t.palette.mode === "light"
 								? t.palette.grey[50]
 								: t.palette.grey[900],
-						backgroundSize: "cover",
+						backgroundSize: "70% auto",
 						backgroundPosition: "center",
 					}}
 				/>
@@ -72,11 +73,8 @@ export default function SignInSide() {
 							alignItems: "center",
 						}}
 					>
-						<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-							<LockOutlinedIcon />
-						</Avatar>
 						<Typography component="h1" variant="h5">
-							Connecter
+							Connecter à votre programme
 						</Typography>
 						<Box
 							component="form"
@@ -84,50 +82,23 @@ export default function SignInSide() {
 							onSubmit={handleSubmit}
 							sx={{ mt: 1 }}
 						>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								id="email"
-								label="Email Address"
-								name="email"
-								autoComplete="email"
-								autoFocus
-							/>
-							<TextField
-								margin="normal"
-								required
-								fullWidth
-								name="password"
-								label="Password"
-								type="password"
-								id="password"
-								autoComplete="current-password"
-							/>
-							<FormControlLabel
-								control={<Checkbox value="remember" color="primary" />}
-								label="Remember me"
-							/>
 							<Button
-								type="submit"
+								type="Button"
 								fullWidth
 								variant="contained"
 								sx={{ mt: 3, mb: 2 }}
 							>
-								Sign In
+								WHP
 							</Button>
-							<Grid container>
-								<Grid item xs>
-									<Link href="#" variant="body2">
-										Forgot password?
-									</Link>
-								</Grid>
-								<Grid item>
-									<Link href="#" variant="body2">
-										{"Don't have an account? Sign Up"}
-									</Link>
-								</Grid>
-							</Grid>
+							<Button
+								type="Button"
+								fullWidth
+								variant="contained"
+								sx={{ mt: 3, mb: 2 }}
+							>
+								VIH SIDA
+							</Button>
+
 							<Copyright sx={{ mt: 5 }} />
 						</Box>
 					</Box>
